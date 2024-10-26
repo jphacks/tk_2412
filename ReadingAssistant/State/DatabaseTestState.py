3f088e5673555e2d3e28fad3b6a32f523e561a1d
evyde <life.hanfeng@hotmail.com>
2024-10-26 19:05:35 +0900

    feat(backend/State/DatabaseTestState): Temp test.

---
+++ ReadingAssistant/State/DatabaseTestState.py
@@ -0,0 +1,15 @@
+import asyncio
+from datetime import datetime
+
+import reflex as rx
+
+from ReadingAssistant.Model.EmotionModel import Emotion
+from ReadingAssistant.State.EmotionState import EmotionState
+
+
+class DatabaseTestState(rx.State):
+    async def fetch_data(self):
+        while True:
+            with rx.session() as session:
+                emotion = session.exec(Emotion.select().order_by(Emotion.timestamp.desc())).first()
+                print("{} @ {}".format(emotion.value, emotion.timestamp))
